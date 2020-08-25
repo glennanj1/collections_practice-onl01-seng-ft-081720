461dc92bf2e612681cccd90a6c854a790e9c1bf1
John Glennan <glennan20@gmail.com>
2020-08-25 14:29:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,4 +34,12 @@ def sum_array (array)
   array.inject {|sum, num| sum + num}
 end  
 
-def add_s()
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
